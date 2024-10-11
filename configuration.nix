ad248378633f7ce3c94f4f9f72d888d8127d1be1
Alan <wuup@users.noreply.github.com>
2024-10-11 09:18:28 -0400

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,150 +1,128 @@
-# Welcome to my NixOS config
-
-{ config, pkgs, ... }:
-
-let
-  userConfig = {
-    name = "debug";
-    homeDir = "/home/debug";
-    description = "Debug user";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      wget
-      appimage-run
-      signal-desktop
-      gh
-      nix
-      discord
-      slack
-      steam
-      brave
-      zoom-us
-      ulauncher
-      obs-studio
-      vlc
-      neofetch
-      audacity
-      obsidian
-      teamspeak_client
-      libreoffice
-    ];
-  };
-in
+{ config, pkgs, lib, ... }:
 
 {
-  # Introducing the computer to its hardware.
   imports = [ ./hardware-configuration.nix ];
 
-  # Setting up GRUB for OS selection at boot.
-  boot.loader.grub = {
-    enable = true;
-    device = "/dev/sda";
-    useOSProber = true;
-  };
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-  # Network setup with a name and NetworkManager for easy internet access.
-  networking = {
-    hostName = "nixos";
-    networkmanager.enable = true;
-  };
-
-  # Locale and time zone to keep your computer in the loop.
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+  networking.firewall.enable = true;
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ]; 
+  
   time.timeZone = "America/New_York";
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
-  # Desktop and login manager setup for a nice GUI.
-  services.xserver = {
-    enable = true;
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.ntp.enable = true;
+
+  services.xserver.xkb = {
     layout = "us";
-    xkbVariant = "";
-    displayManager.lightdm.enable = true;
-    desktopManager.pantheon.enable = true;
+    variant = "";
   };
 
-  # Configuring system services like printing and sound.
-  services = {
-    printing.enable = true;
-    pipewire = {
-      enable = true;
-      alsa = { enable = true; support32Bit = true; };
-      pulse.enable = true;
-    };
-  };
+  services.printing.enable = false;
 
-  # Sound system with Pipewire and real-time audio privileges.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-
-  # User account setup.
-  users.users.${userConfig.name} = {
-    isNormalUser = true;
-    home = userConfig.homeDir;
-    createHome = true;
-    description = userConfig.description;
-    extraGroups = userConfig.extraGroups;
-    packages = userConfig.packages;
+  
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
   };
 
-  # Allowing 'unfree' packages and defining system-wide essentials.
+  users.users.debug = {
+    isNormalUser = true;
+    description = "Alan Hamlyn";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  programs.firefox.enable = true;
   nixpkgs.config.allowUnfree = true;
+
   environment.systemPackages = with pkgs; [
-    vim
-    ripgrep
-    fd
-    wget
-    appimage-run
+    firefox
+    libreoffice
+    gnome.gnome-terminal
+    gparted
+    audacity
+    vlc
+    remmina
+    gnome.gnome-disk-utility
+    evince
     git
-    mongodb
-    htop
-    python3
-    python311Packages.pip
+    brave
+    discord
+    google-chrome
+    mongodb-compass
+    teamspeak_client
+    steam
+    steam-run
+    vscode
+    obsidian
+    chromium
+    zoom-us
+    thunderbird
+    signal-desktop
+    protonmail-bridge
+    vim
   ];
 
-  # Systemd setup for services and tasks.
-  systemd = {
-    tmpfiles.rules = [ "d /home/debug/Applications 0755 debug users - -" ];
-    services.cursorAppImage = {
-      description = "Sets up Cursor AppImage for 'debug'.";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-      path = [ pkgs.wget pkgs.coreutils ];
-      script = ''
-        mkdir -p /home/debug/Applications
-        ${pkgs.wget}/bin/wget -O /home/debug/Applications/cursor-0.28.1-x86_64.AppImage https://download.cursor.sh/linux/appImage/x64
-        chmod +x /home/debug/Applications/cursor-0.28.1-x86_64.AppImage
-      '';
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-        User = "debug";
-      };
-    };
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.powerManagement.enable = true;
+  hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.extraPackages = with pkgs.pkgsi686Linux; [ libGL vulkan-loader ];
+
+  services.flatpak.enable = true;
+  virtualisation.virtualbox.host.enable = true;
+  services.mongodb.enable = true;
+
+  boot.kernelParams = [ "zswap.enabled=1" "zswap.compressor=lz4" "zswap.max_pool_percent=20" "kernel.perf_event_paranoid=2" "quiet" ];
+
+  fileSystems."/tmp" = {
+    device = "tmpfs";
+    fsType = "tmpfs";
+    options = [ "size=4G" "mode=1777" ];
   };
 
-  # AppImage support setup.
-  boot.binfmt.registrations.appimage = {
-    wrapInterpreterInShell = false;
-    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
-    recognitionType = "magic";
-    offset = 0;
-    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
-    magicOrExtension = ''\x7fELF....AI\x02'';
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/edd63e45-be15-42f1-b2b1-026d039720de";
+    fsType = "ext4";
+    options = [ "noatime" "discard" ];
   };
 
-  # System version for upgrades and troubleshooting.
-  system.stateVersion = "23.11";
+  powerManagement.cpuFreqGovernor = "schedutil";
+
+  services.journald.extraConfig = ''
+    SystemMaxUse=200M
+    RuntimeMaxUse=100M
+    MaxFileSec=1day
+  '';
+
+  nix.settings.cores = 0;
+
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 20;
+  };
+
+  system.stateVersion = "24.05";
 }
